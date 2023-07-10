c90756c3d37d5f83b39ddee75a0ef68ec5421caa
jepson <jiangpeng@itcast.cn>
2023-07-11 07:30:49 +0800

    引入element-plus

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,12 @@ const goList = () => {
 <template>
   <div>
     我是App
-    <button @click="$router.push('/home')">跳首页</button>
-    <button @click="goList">跳列表页</button>
+    <test-demo></test-demo>
+    <el-button @click="$router.push('/home')">跳首页</el-button>
+    <el-button @click="goList">跳列表页</el-button>
+
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
   </div>
 </template>
 
