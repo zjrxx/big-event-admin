e31a572b33483d7858dc61e62c26524f533617ba
jepson <jiangpeng@itcast.cn>
2023-07-11 06:41:03 +0800

    路由模块分析理解

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+// 在 Vue3 CompositionAPI 中
+// 1. 获取路由对象 router  useRouter
+//    const router = useRouter()
+// 2. 获取路由参数 route   useRoute
+//    const route = useRoute()
+import { useRoute, useRouter } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
+</script>
 
 <template>
-  <div>我是App</div>
+  <div>
+    我是App
+    <button @click="$router.push('/home')">跳首页</button>
+    <button @click="goList">跳列表页</button>
+  </div>
 </template>
 
 <style scoped></style>
